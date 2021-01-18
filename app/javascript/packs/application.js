679bcff6c2e4f4fddec81ab68cf77dcb0417591c
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2021-01-18 17:49:00 +0100

    Basic project skeleton with tailwind

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,10 @@
+import '../stylesheets/application.scss';
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      blue: colors.blue,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
+
