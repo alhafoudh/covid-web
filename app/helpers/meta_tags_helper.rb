6ac7677b278dfa34c91fdc0286f73e79ae68e2e1
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2021-01-19 13:58:14 +0100

    Add og:image meta tag

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -6,4 +6,9 @@ module MetaTagsHelper
   def meta_description
     content_for?(:meta_description) ? content_for(:meta_description) : t(:meta_description)
   end
+
+  def meta_image
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : image_path('og_image.png'))
+    meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
+  end
 end
