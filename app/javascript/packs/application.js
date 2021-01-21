52e9ad00fb73c07b80dee497717f86e61a278041
Valter Martinek <martinek@freevision.sk>
2021-01-21 14:24:53 +0100

    Remove unused javascripts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,3 @@
 import '../stylesheets/application.scss';
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-ActiveStorage.start()
-
 import "controllers"
