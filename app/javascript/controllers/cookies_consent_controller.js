23d8e86688b24892f8b2d39002ecb5ad4c7bdee6
Valter Martinek <martinek@freevision.sk>
2021-01-22 19:17:00 +0100

    Remove marketing cookies consent, update layout and texts

--- app/javascript/controllers/cookies_consent_controller.js
+++ app/javascript/controllers/cookies_consent_controller.js
@@ -2,7 +2,7 @@ import {Controller} from "stimulus"
 
 export default class extends Controller {
   static configurationLocalStorageKey = "cookies";
-  static targets = ["container", "configPanel", "analyticsCheckbox", "marketingCheckbox", "acceptAllButton", "saveConfigurationButton"];
+  static targets = ["container", "info", "configPanel", "analyticsCheckbox", "acceptAllButton", "saveConfigurationButton"];
 
   connect() {
     const configuration = JSON.parse(localStorage.getItem(this.constructor.configurationLocalStorageKey));
@@ -11,49 +11,43 @@ export default class extends Controller {
       this.initializeScripts(configuration);
     } else {
       setTimeout(() => {
-        this.toggle();
+        this.toggle(true);
       }, 1000);
     }
   }
 
   acceptAll() {
-    const configuration = {analytic: true, marketing: true}
+    const configuration = {analytic: true}
     this.saveConfiguration(configuration)
   }
 
   toggleConfigurationForm() {
     this.configPanelTarget.classList.toggle('hidden')
+    this.infoTarget.classList.toggle('hidden')
     this.acceptAllButtonTarget.classList.toggle('hidden')
     this.saveConfigurationButtonTarget.classList.toggle('hidden')
   }
 
   saveConfigurationForm() {
     const analytic = this.analyticsCheckboxTarget.checked;
-    const marketing = this.marketingCheckboxTarget.checked;
-    const configuration = {analytic, marketing};
+    const configuration = {analytic};
     this.saveConfiguration(configuration);
   }
 
   saveConfiguration(configuration) {
     localStorage.setItem(this.constructor.configurationLocalStorageKey, JSON.stringify(configuration));
     this.initializeScripts(configuration);
-    this.toggle();
+    this.toggle(false);
   }
 
   initializeScripts(configuration) {
-    console.log('init with configuration', configuration);
     if (configuration.analytic) {
-      console.log('trigger analytic consent')
       window.dataLayer.push({event: 'consentAnalyticCookies'})
-    }
-    if (configuration.marketing) {
-      console.log('trigger marketing consent')
-      window.dataLayer.push({event: 'consentMarketingCookies'})
     }
   }
 
-  toggle() {
-    this.containerTarget.classList.toggle('translate-y-1/2')
-    this.containerTarget.classList.toggle('opacity-100')
+  toggle(show) {
+    this.containerTarget.classList.toggle('translate-y-1/2', !show)
+    this.containerTarget.classList.toggle('opacity-100', show)
   }
 }
