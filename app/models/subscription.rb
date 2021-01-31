63fa246be3d0f74c5dcd858d292fcdfcfd2711c5
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2021-01-31 21:00:34 +0100

    Implement deliver method on Subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,4 +5,21 @@ class Subscription < ApplicationRecord
 
   validates :channel, presence: true
   validates :user_id, presence: true
+
+  def deliver(text)
+    case channel
+    when 'messenger' then
+      Bot.deliver({
+        recipient: {
+          id: user_id
+        },
+        message: {
+          text: text
+        },
+        messaging_type: Facebook::Messenger::Bot::MessagingType::UPDATE
+      }, page_id: Rails.application.config.x.messenger.page_id)
+    else
+      raise NotImplementedError
+    end
+  end
 end
