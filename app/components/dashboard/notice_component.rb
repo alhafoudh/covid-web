da470d514a3a31f7e5dc2fcdcabd5a0c161d1a54
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2021-02-04 22:56:03 +0100

    Refactor notices to component

---
+++ app/components/dashboard/notice_component.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Dashboard::NoticeComponent < ViewComponent::Base
+  attr_reader :type, :content
+
+  def initialize(type:, content:)
+    @type = type
+    @content = content
+  end
+
+  def render?
+    content.present? && !content.empty?
+  end
+
+  def icon
+    case type
+    when :warning then '❗️'
+    else
+      'ℹ️'
+    end
+  end
+
+  def classes
+    case type
+    when :warning then 'text-red-600 bg-red-100'
+    else
+      'text-blue-600 bg-blue-100'
+    end
+  end
+end
