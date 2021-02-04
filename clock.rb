143963650e3a4ce73e0651641b9a6f3b4dbef9b0
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2021-02-04 11:50:13 +0100

    Enable updating MOM in clock

--- clock.rb
+++ clock.rb
@@ -5,15 +5,15 @@ require './config/environment'
 Thread.report_on_exception = true
 
 module Clockwork
-  # every(Rails.application.config.x.testing.update_interval.minutes, 'update_all_test_date_snapshots') do
-  #   SkCovidTesting::Application.load_tasks
-  #
-  #   UpdateNcziMoms.new.perform
-  #   RychlejsieMom.instances.map do |config|
-  #     UpdateRychlejsieMoms.new(config).perform
-  #   end
-  #   UpdateAllTestingSnapshots.new(rate_limit: Rails.application.config.x.testing.rate_limit).perform
-  # end
+  every(Rails.application.config.x.testing.update_interval.minutes, 'update_all_test_date_snapshots') do
+    SkCovidTesting::Application.load_tasks
+
+    UpdateNcziMoms.new.perform
+    RychlejsieMom.instances.map do |config|
+      UpdateRychlejsieMoms.new(config).perform
+    end
+    UpdateAllTestingSnapshots.new(rate_limit: Rails.application.config.x.testing.rate_limit).perform
+  end
 
   every(Rails.application.config.x.vaccination.update_interval.minutes, 'update_all_vaccination_date_snapshots') do
     SkCovidTesting::Application.load_tasks
