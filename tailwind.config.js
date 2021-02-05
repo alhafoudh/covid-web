8f5fecf4952f23a8d0e919172477311f51768733
Valter Martinek <martinek@freevision.sk>
2021-02-05 15:52:06 +0100

    Update typography, change grays

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
   ],
   corePlugins: {
     backgroundOpacity: false,
+    borderOpacity: false,
     textOpacity: false,
   },
   darkMode: false, // or 'media' or 'class'
@@ -23,7 +24,11 @@ module.exports = {
       current: 'currentColor',
       black: colors.black,
       white: colors.white,
-      gray: colors.trueGray,
+      gray: {
+        dark: '#838383',
+        DEFAULT: '#aaaaaa',
+        light: '#f0f0f0',
+      },
       indigo: colors.indigo,
       red: {
         DEFAULT: '#e00000',
@@ -42,9 +47,41 @@ module.exports = {
       },
       orange: colors.orange,
     },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': ['2.375rem', {lineHeight: '1.31em'}],
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
     extend: {
       spacing: {
         30: '7.5rem'
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              fontWeight: 300,
+            },
+            h2: {
+              fontWeight: 300,
+            },
+            h3: {
+              fontWeight: 300,
+            },
+            h4: {
+              fontWeight: 300,
+            },
+          }
+        }
       }
     },
   },
