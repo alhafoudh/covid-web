0382b3124c1886ff9c9d4339fdcd577d884b895e
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2021-02-08 14:57:37 +0100

    Rework NoticeComponent

--- app/components/dashboard/notice_component.rb
+++ app/components/dashboard/notice_component.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class Dashboard::NoticeComponent < ViewComponent::Base
-  attr_reader :type, :content
+  attr_reader :type, :content, :classes, :class_name
 
-  def initialize(type:, content:)
+  def initialize(type: :info, icon: nil, content:, class_name: '')
     @type = type
+    @icon = icon
     @content = content
+    @class_name = class_name
   end
 
   def render?
@@ -13,10 +15,25 @@ class Dashboard::NoticeComponent < ViewComponent::Base
   end
 
   def classes
-    case type
-    when :warning then 'text-red bg-red-light'
+    class_names(
+      class_name,
+      'text-red bg-red-light': type == :danger,
+      'text-yellow bg-yellow-light': type == :warning,
+      'text-green bg-green-light': type == :success,
+      'text-blue bg-blue-light': type == :info,
+    )
+  end
+
+  def icon
+    if @icon.present?
+      @icon
     else
-      'text-blue bg-blue-light'
+      case type
+      when :warning then
+        :exclamation
+      else
+        :info
+      end
     end
   end
 end
