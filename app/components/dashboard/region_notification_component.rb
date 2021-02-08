38aea81602c8fadc5e08a2ec2444a30e796ca379
Valter Martinek <martinek@freevision.sk>
2021-02-08 19:51:36 +0100

    Wrap items better on mobile screens

--- app/components/dashboard/region_notification_component.rb
+++ app/components/dashboard/region_notification_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Dashboard::RegionNotificationComponent < ViewComponent::Base
-  attr_reader :region
+  attr_reader :region, :class_name
 
-  def initialize(region:)
+  def initialize(region:, class_name: nil)
     @region = region
+    @class_name = class_name
   end
 
   def render?
