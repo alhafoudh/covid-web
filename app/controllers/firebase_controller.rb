43be5eef7da84e0922cea16efea63c7a39a33b9a
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2021-02-12 19:41:39 +0100

    Update FirebaseController configuration http cache control options

--- app/controllers/firebase_controller.rb
+++ app/controllers/firebase_controller.rb
@@ -2,6 +2,6 @@ class FirebaseController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def configuration
-    expires_in 1.minute
+    expires_in(Rails.application.config.x.cache.content_expiration_minutes, public: true, stale_while_revalidate: Rails.application.config.x.cache.content_stale_minutes)
   end
 end
