115dbe4a5c02db0019955f574ed88e702654a06c
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2021-05-17 11:42:39 +0200

    Disable testing page

--- app/controllers/testing/dashboard_controller.rb
+++ app/controllers/testing/dashboard_controller.rb
@@ -1,25 +1,6 @@
 module Testing
-  class DashboardController < ::DashboardController
-    private
-
-    def plan_date_snapshot_foreign_key
-      :test_date_id
-    end
-
-    def place_model
-      Mom
-    end
-
-    def places_association
-      :moms
-    end
-
-    def plan_date_model
-      TestDate
-    end
-
-    def num_plan_date_days
-      Rails.application.config.x.testing.num_plan_date_days
+  class DashboardController < ::ApplicationController
+    def index
     end
   end
 end
